02c79a8ae010c182671effdcef86fc9d22dc54e0
Anony <noreply@anonyindian.me>
2022-02-23 00:43:10 +0530

    Add README for all topics

--- src/ext/context.rs
+++ src/ext/context.rs
@@ -1,50 +1,84 @@
-use crate::types::{User, Chat, Message, Update, CallbackQuery, InlineQuery};
+use crate::types::{User, Chat, Message, Update};
 
 #[derive(Clone)]
 pub struct Context {
     pub effective_user: Option<User>,
-    pub effective_chat: Chat,
+    pub effective_chat: Option<Chat>,
     pub effective_message: Option<Box<Message>>,
-    pub callback_query: Option<CallbackQuery>,
-    pub inline_query: Option<InlineQuery>,
-    pub raw_update: Update,
+    pub update: Update,
 }
 
 impl Context {
     pub fn new(update: &Update) -> Self {
-        // let chat = ;
+        
         let udp = update.clone();
         let mut ctx = Self {
             effective_user: None,
-            effective_chat: Chat{
-                id: 0, 
-                r#type: String::new(), 
-                title: Option::default(), 
-                username: Option::default(), 
-                first_name: Option::default(), 
-                last_name: Option::default(), 
-                photo: Option::default(), 
-                bio: Option::default(), 
-                has_private_forwards: Option::default(), 
-                description: Option::default(), 
-                invite_link: Option::default(), 
-                pinned_message: Option::default(), 
-                permissions: Option::default(), 
-                slow_mode_delay: Option::default(), 
-                message_auto_delete_time: Option::default(), 
-                has_protected_content: Option::default(), sticker_set_name: Option::default(), can_set_sticker_set: Option::default(), linked_chat_id: Option::default(), location: Option::default() },
+            effective_chat: None,
             effective_message: None,
-            raw_update: update.clone(),
-            callback_query: None,
-            inline_query: None,
+            update: update.clone(),
         };
         if update.message.is_some() {
+
             let msg = update.clone().message.unwrap();
             ctx.effective_message = udp.message;
-            if msg.from.is_some() {
-                ctx.effective_user = msg.from
-            }
-            ctx.effective_chat = msg.chat
+            ctx.effective_user = msg.from;
+            ctx.effective_chat = Some(msg.chat)
+
+        } else if update.edited_message.is_some() {
+
+            let msg = update.clone().edited_message.unwrap();
+            ctx.effective_message = udp.edited_message;
+            ctx.effective_user = msg.from;
+            ctx.effective_chat = Some(msg.chat)
+
+        } else if update.channel_post.is_some() {
+
+            let msg = update.clone().channel_post.unwrap();
+            ctx.effective_message = udp.channel_post;
+            ctx.effective_chat = Some(msg.chat)
+
+        } else if update.edited_channel_post.is_some() {
+
+            let msg = update.clone().edited_channel_post.unwrap();
+            ctx.effective_message = udp.edited_channel_post;
+            ctx.effective_chat = Some(msg.chat)
+
+        } else if update.inline_query.is_some() {
+
+            let msg = update.clone().inline_query.unwrap();
+            ctx.effective_user = Some(msg.from)
+
+        } else if update.callback_query.is_some() {
+
+            let msg = update.clone().callback_query.unwrap();
+            ctx.effective_message = Some(Box::new(msg.message.unwrap()));
+            ctx.effective_user = Some(msg.from)
+
+        } else if update.chosen_inline_result.is_some() {
+
+            ctx.effective_user = Some(udp.chosen_inline_result.unwrap().from)
+        
+        } else if update.shipping_query.is_some() {
+            
+            ctx.effective_user = Some(udp.shipping_query.unwrap().from)
+        
+        } else if update.pre_checkout_query.is_some() {
+
+            ctx.effective_user = Some(udp.pre_checkout_query.unwrap().from)
+
+        } else if update.my_chat_member.is_some() {
+
+            let msg = update.clone().my_chat_member.unwrap();
+            ctx.effective_user = Some(msg.from);
+            ctx.effective_chat = Some(msg.chat)
+
+        } else if update.chat_join_request.is_some() {
+
+            let msg = update.clone().chat_join_request.unwrap();
+            ctx.effective_user = Some(msg.from);
+            ctx.effective_chat = Some(msg.chat)
+
         }
         return ctx;
     }
---
+++ src/ext/handlers/README.md
@@ -0,0 +1,13 @@
+# **FretGram**
+## **ext::handlers**
+This directory contains the handlers of the fretgram library which you can use to answer the updates.
+
+## **Contributing**
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update examples as appropriate.
+
+
+## **License**
+[![MIT](https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/MIT_logo.svg/200px-MIT_logo.svg.png)](https://opensource.org/licenses/MIT)
+<br>Licensed Under <a href="https://opensource.org/licenses/MIT">The MIT License</a>
