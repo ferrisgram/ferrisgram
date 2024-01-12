2d1b7e94a96736f79f40bd64b777911f226c8633
celestix <73958752+celestix@users.noreply.github.com>
2024-01-13 01:52:52 +0530

    handle flood waits

--- src/bot.rs
+++ src/bot.rs
@@ -1,6 +1,6 @@
 use crate::error::{Error, Result};
 use crate::input_file::InputFile;
-use crate::types::User;
+use crate::types::{User, ResponseParameters};
 use crate::DEFAULT_API_URL;
 use reqwest::multipart::Form;
 use reqwest::Client;
@@ -9,6 +9,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
 use std::time::Duration;
+
 
 #[derive(Debug, Clone)]
 pub struct Bot {
@@ -24,6 +25,7 @@ pub struct ApiResponse<T> {
     pub error_code: Option<i32>,
     pub description: Option<String>,
     pub result: Option<T>,
+    pub parameters: Option<ResponseParameters>
 }
 
 impl Bot {
@@ -55,20 +57,28 @@ impl Bot {
         method: &str,
         params: Option<&serde_json::Value>,
     ) -> Result<T> {
-        let mut resp = self.client.get(format!(
-            "{url}/bot{token}/{method}",
-            url = self.api_url,
-            token = self.token,
-            method = method
-        ));
-        if params.is_some() {
-            resp = resp.json(&params);
-        }
-        let resp = resp.send().await?.json::<ApiResponse<T>>().await?;
-        if resp.ok {
-            Ok(resp.result.unwrap())
-        } else {
-            Err(Error::TelegramError(resp.description.unwrap()))
+        loop {
+            let mut resp = self.client.get(format!(
+                "{url}/bot{token}/{method}",
+                url = self.api_url,
+                token = self.token,
+                method = method
+            ));
+            if params.is_some() {
+                resp = resp.json(&params);
+            }
+            let resp = resp.send().await?.json::<ApiResponse<T>>().await?;
+            if resp.ok {
+                return Ok(resp.result.unwrap());
+            } else {
+                if let Some(resp_params) = resp.parameters {
+                    if let Some(retry_after) = resp_params.retry_after {
+                        tokio::time::sleep(tokio::time::Duration::from_secs(retry_after.unsigned_abs())).await;
+                        continue;
+                    }
+                }
+                return Err(Error::TelegramError(resp.description.unwrap()));
+            }
         }
     }
     pub async fn post<T: DeserializeOwned, F: InputFile>(
@@ -77,37 +87,45 @@ impl Bot {
         params: Option<&serde_json::Value>,
         data: Option<HashMap<&str, F>>,
     ) -> Result<T> {
-        let mut resp = self.client.post(format!(
-            "{url}/bot{token}/{method}",
-            url = self.api_url,
-            token = self.token,
-            method = method
-        ));
-        // Use multipart if data is not none else use JSON
-        if data.is_some() {
-            let mut form = Form::new();
-            for (name, field) in data.unwrap() {
-                form = form.part(name.to_string(), field.get_part())
-            }
-            if let Value::Object(obj) = params.unwrap().clone() {
-                for (key, val) in obj {
-                    if let Value::String(s) = val {
-                        form = form.text(key, s);
-                    } else {
-                        form = form.text(key, val.to_string())
+        loop {
+            let mut resp = self.client.post(format!(
+                "{url}/bot{token}/{method}",
+                url = self.api_url,
+                token = self.token,
+                method = method
+            ));
+            // Use multipart if data is not none else use JSON
+            if data.is_some() {
+                let mut form = Form::new();
+                for (name, field) in data.as_ref().unwrap() {
+                    form = form.part(name.to_string(), field.get_part())
+                }
+                if let Value::Object(obj) = params.unwrap().clone() {
+                    for (key, val) in obj {
+                        if let Value::String(s) = val {
+                            form = form.text(key, s);
+                        } else {
+                            form = form.text(key, val.to_string())
+                        }
                     }
                 }
+                resp = resp.multipart(form);
+                // form.part("", part);
+            } else if params.is_some() {
+                resp = resp.json(&params);
             }
-            resp = resp.multipart(form);
-            // form.part("", part);
-        } else if params.is_some() {
-            resp = resp.json(&params);
-        }
-        let resp = resp.send().await?.json::<ApiResponse<T>>().await?;
-        if resp.ok {
-            Ok(resp.result.unwrap())
-        } else {
-            Err(Error::TelegramError(resp.description.unwrap()))
+            let resp = resp.send().await?.json::<ApiResponse<T>>().await?;
+            if resp.ok {
+                return Ok(resp.result.unwrap());
+            } else {
+                if let Some(resp_params) = resp.parameters {
+                    if let Some(retry_after) = resp_params.retry_after {
+                        tokio::time::sleep(tokio::time::Duration::from_secs(retry_after.unsigned_abs())).await;
+                        continue;
+                    }
+                }
+                return Err(Error::TelegramError(resp.description.unwrap()));
+            }
         }
     }
 }
